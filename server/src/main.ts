7c04c67136e7c7531bccf6090a2677e8f8dc2e71
Lord Drake <jsparn@gmail.com>
2021-03-14 20:42:05 -0400

    Added winston for logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import express from "express";
 
+import Logger from "./modules/logging/logger";
+
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(PORT, () => {
-  console.log(`⚡[Server]: WaW server is running at http://localhost:${PORT}`);
+  Logger.debug(`⚡[Server]: WaW server is running at http://localhost:${PORT}`);
 });

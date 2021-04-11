b79385f3310f7489af93880fa21185940e0955f5
Lord Drake <jsparn@gmail.com>
2021-04-11 12:08:31 -0400

    Cleaned up commented code and added error logging for DB connection

--- server/src/services/db-service.ts
+++ server/src/services/db-service.ts
@@ -3,8 +3,6 @@ import mongoose from "mongoose";
 import Config from "../modules/config/config";
 import Logger from "../modules/logging/logger";
 
-import Country from "../models/country.model";
-
 export class DbService {
   private conf: Config;
 
@@ -15,37 +13,13 @@ export class DbService {
   public async ConnectToDb() {
     const uri = `mongodb+srv://${this.conf.DB.User}:${this.conf.DB.Password}@${this.conf.DB.Host}?authSource=${this.conf.DB.AuthSource}&retryWrites=true&w=majority`;
 
-    mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    try {
+      mongoose.connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+    } catch (err: any) {
+      Logger.error(`Unable to establish DB connection: ${err.message}`);
+    }
   }
-
-  // public async getDbConnection() {
-  //   const uri = `mongodb+srv://${this.conf.DB.User}:${this.conf.DB.Password}@${this.conf.DB.Host}?authSource=${this.conf.DB.AuthSource}&retryWrites=true&w=majority`;
-
-  //   mongoose.connect(uri);
-
-  //   let db = mongoose.connection;
-
-  //   db.on("error", (err: Error) =>
-  //     Logger.error(
-  //       `Unable to establish connection to the database:\n\t${err.name}:\n\t${err.message}\n` +
-  //         "-".repeat(80)
-  //     )
-  //   );
-  //   db.once("open", () => Logger.debug("It works?"));
-
-  //   const country = new Country({
-  //     _id: "abc",
-  //     government: "baguette",
-  //     headOfGovernment: "jake",
-  //     name: "france",
-  //     party: "honhon",
-  //     status: "surrendering",
-  //   });
-
-  //   country.save((err, country) => {
-  //     if (err) {
-  //       Logger.error(`Well this is borked: ${err}`);
-  //     }
-  //   });
-  // }
 }
